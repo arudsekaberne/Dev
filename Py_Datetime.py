615fe69d7ec00babdd1045bab56e7adfa37b26fa
arudsekaberne <68807002+arudsekaberne@users.noreply.github.com>
2023-11-11 21:06:04 +0530

    Added Date Timezone conversion

--- Py_Datetime.py
+++ Py_Datetime.py
@@ -1,3 +1,4 @@
+import pytz
 from datetime import datetime
 
 
@@ -9,3 +10,14 @@ ext_timezone = ext_datetime.tzinfo
 print("Current Datetime: {}".format(cur_datetime))
 print("Extended Datetime: {}".format(ext_datetime))
 print("Timezone: {}".format(ext_timezone))
+
+# 2. Convert datetime to another timezone
+
+ist_timezone = pytz.timezone("Asia/Calcutta")
+pst_timezone = pytz.timezone("America/Los_Angeles")
+
+ist_datetime = ist_timezone.localize(cur_datetime)
+pst_datetime = ist_datetime.astimezone(pst_timezone)
+
+print("IST Date Time: {}".format(ist_datetime))
+print("PST Date Time: {}".format(pst_datetime))
